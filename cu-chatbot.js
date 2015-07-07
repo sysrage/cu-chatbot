48b3d0073763765ad22dfbce9a4c6c5b559071a0
sysrage <sysrage@gmail.com>
2015-07-06 20:13:25 -0400

    Fully functional MOTD features and minor cleanup.

--- cu-chatbot.js
+++ cu-chatbot.js
@@ -3,7 +3,7 @@
 
 // Requires node-xmpp
 // Based on https://gist.github.com/powdahound/940969
-// Much thanks to Mehuge for extensive help.
+// Much thanks to Mehuge, reallifegobbo, and burfo for extensive help.
 
 var sys = require('sys');
 var util = require('util');
@@ -17,24 +17,29 @@ var config = require('./cu-chatbot.cfg');
 var commandChar = '!';
 var chatCommands = [
 {
-    command: 'motdtest',
+    command: 'motd',
     exec: function(client, server, room, sender, message, extras) {
-        var params = message.replace(/^!motdtest[\ ]*/, '');
+        var params = message.replace(/^.motd[\ ]*/, '');
         if (extras && extras.motdadmin) {
             var motdadmin = extras.motdadmin;
         } else {
-            var motdadmin = 'no';
+            var motdadmin = false;
         }
 
         if (params.length > 0) {
             // User is trying to set a new MOTD.
-            if (motdadmin === 'yes') {
+            if (motdadmin) {
                 // User is allowed - Set new MOTD.
                 fs.writeFile(server.motdfile, "MOTD: " + params, function(err) {
                     if (err) {
-                        return util.log('[ERROR] Unable to write to MOTD file.');
+                        return util.log("[ERROR] Unable to write to MOTD file.");
                     }
                     server.motd = "MOTD: " + params;
+                    if (room === 'pm') {
+                        sendPM(client, server, "MOTD for " + server.name + " set to: " + params, sender);
+                    } else {
+                        sendChat(client, server, "MOTD for " + server.name + " set to: " + params, room);
+                    }
                     util.log("[MOTD] New MOTD for server '" + server.name + "' set by user '" + sender + "'.");
                 });
             } else {
@@ -49,10 +54,78 @@ var chatCommands = [
             // User requested current MOTD.
             if (room === 'pm') {
                 sendPM(client, server, server.motd.toString(), sender);
-                util.log("[MOTD] MOTD sent to user '" + sender + "'.");
+                util.log("[MOTD] MOTD sent to user '" + sender + "' on " + server.name + ".");
             } else {
                 sendChat(client, server, server.motd.toString(), room);
                 util.log("[MOTD] MOTD sent to '" + server.name + '/' + room.split('@')[0] + "' per user '" + sender + "'.");
+            }
+        }
+    }
+},
+{
+    command: 'motdoff',
+    exec: function(client, server, room, sender, message, extras) {
+        var ignoredReceiver = false;
+        server.motdIgnore.forEach(function(receiver) {
+            if (receiver == sender) ignoredReceiver = true;
+        });
+
+        if (! ignoredReceiver) {
+            // Add user to MOTD ignore list
+            server.motdIgnore.push(sender);
+            fs.writeFile(server.nomotdfile, JSON.stringify(server.motdIgnore), function(err) {
+                if (err) {
+                    return util.log("[ERROR] Unable to write to MOTD Ignore file.");
+                }
+                if (room === 'pm') {
+                    sendPM(client, server, "User '" + sender + "' unsubscribed from " + server.name + " MOTD notices.", sender);
+                } else {
+                    sendChat(client, server, "User '" + sender + "' unsubscribed from " + server.name + " MOTD notices.", room);
+                }
+                util.log("[MOTD] User '" + sender + "' added to '" + server.name + "' opt-out list.");
+            });
+        } else {
+            // Tell user they already have MOTDs turned off
+            if (room === 'pm') {
+                sendPM(client, server, "User '" + sender + "' already unsubscribed from " + server.name + " MOTD notices.", sender);
+            } else {
+                sendChat(client, server, "User '" + sender + "' already unsubscribed from " + server.name + " MOTD notices.", room);
+            }
+        }
+    }
+},
+{
+    command: 'motdon',
+    exec: function(client, server, room, sender, message, extras) {
+        var ignoredReceiver = false;
+        server.motdIgnore.forEach(function(receiver) {
+            if (receiver == sender) ignoredReceiver = true;
+        });
+
+        if (! ignoredReceiver) {
+            // Remove user to MOTD ignore list
+            for (var i = 0; i < server.motdIgnore.length; i++) {
+                if (server.motdIgnore[i] === sender) index = i;
+            }
+            server.motdIgnore.splice(index, 1);
+
+            fs.writeFile(server.nomotdfile, JSON.stringify(server.motdIgnore), function(err) {
+                if (err) {
+                    return util.log("[ERROR] Unable to write to MOTD Ignore file.");
+                }
+                if (room === 'pm') {
+                    sendPM(client, server, "User '" + sender + "' subscribed to " + server.name + " MOTD notices.", sender);
+                } else {
+                    sendChat(client, server, "User '" + sender + "' subscribed to " + server.name + " MOTD notices.", room);
+                }
+                util.log("[MOTD] User '" + sender + "' removed from '" + server.name + "' opt-out list.");
+            });
+        } else {
+            // Tell user they already have MOTDs turned on
+            if (room === 'pm') {
+                sendPM(client, server, "User '" + sender + "' already subscribed to " + server.name + " MOTD notices.", sender);
+            } else {
+                sendChat(client, server, "User '" + sender + "' already subscribed to " + server.name + " MOTD notices.", room);
             }
         }
     }
@@ -60,16 +133,6 @@ var chatCommands = [
 ];
 
 /*****************************************************************************/
-
-// function to send a message to a group chat
-var sendChat = function(client, server, message, room) {
-    client[server.name].send(new xmpp.Element('message', { to: room + '/' + server.nickname, type: 'groupchat' }).c('body').t(message));
-};
-
-// function to send a private message
-var sendPM = function(client, server, message, user) {
-    client[server.name].send(new xmpp.Element('message', { to: user, type: 'chat' }).c('body').t(message));
-};
 
 // function to find the index of a room
 var indexOfRoom = function(client, server, room) {
@@ -79,14 +142,24 @@ var indexOfRoom = function(client, server, room) {
     return -1;
 };
 
-var getMOTD = function(server) {
+// function to send a message to a group chat
+function sendChat(client, server, message, room) {
+    client[server.name].send(new xmpp.Element('message', { to: room + '/' + server.nickname, type: 'groupchat' }).c('body').t(message));
+}
+
+// function to send a private message
+function sendPM(client, server, message, user) {
+    client[server.name].send(new xmpp.Element('message', { to: user, type: 'chat' }).c('body').t(message));
+}
+
+function getMOTD(server) {
     fs.readFile(server.motdfile, function(err, data) {
         if (err && err.code === 'ENOENT') {
             fs.writeFile(server.motdfile, "MOTD: ", function(err) {
                 if (err) {
-                    return util.log('[ERROR] Unable to create MOTD file.');
+                    return util.log("[ERROR] Unable to create MOTD file.");
                 }
-                util.log('[STATUS] MOTD file did not exist. Empty file created.');
+                util.log("[STATUS] MOTD file did not exist. Empty file created.");
             });
             server.motd = "MOTD: ";
         } else {
@@ -95,19 +168,68 @@ var getMOTD = function(server) {
     });
 }
 
-// Timers
-// function foobar(el) { setTimeout(function() { foobar_cont(el); }, 5000); }
-// you need to give a var to the settimeout, so like snafu=setTimeout(...)  and then run clearTimeout(snafu);  to remove it
+function getMOTDIgnore(server) {
+    fs.readFile(server.nomotdfile, function(err, data) {
+        if (err && err.code === 'ENOENT') {
+            server.motdIgnore = [];
+            fs.writeFile(server.nomotdfile, JSON.stringify(server.motdIgnore), function(err) {
+                if (err) {
+                    return util.log("[ERROR] Unable to create MOTD Ignore file.");
+                }
+                util.log("[STATUS] MOTD Ignore file did not exist. Empty file created.");
+            });
+        } else {
+            server.motdIgnore = JSON.parse(data);
+        }
+    });
+}
 
+// Timer to send MOTD messages to joining users.
+function timerMOTD(client, server) { setInterval(function() { sendMOTD(client, server); }, 500); }
+function sendMOTD(client, server) {
+    server.motdReceivers.forEach(function(receiver) {
+        epochTime = Math.floor((new Date).getTime() / 1000);
+        if ((epochTime - receiver.joinTime > 2) && receiver.sendTime === 0) {
+            // User joined 2 seconds ago, send the MOTD.
+            receiver.sendTime = epochTime;
+            var user = receiver.name + '@' + server.address;
+            sendPM(client, server, server.motd.toString(), user);
+            util.log("[MOTD] MOTD sent to user '" + receiver.name + "' on " + server.name + ".");
+        } else if ((receiver.sendTime > 0) && (epochTime - receiver.sendTime > 300)) {
+            // User was sent MOTD 5 minutes ago, remove from receiver list so they can get it again.
+            for (var i = 0; i < server.motdReceivers.length; i++) {
+                if (server.motdReceivers[i].name === receiver.name) index = i;
+            }
+            server.motdReceivers.splice(index, 1);
+        }
+    });
+}
+
+var client = [];
 config.servers.forEach(function(server) {
     // Server initialization
     getMOTD(server);
+    getMOTDIgnore(server);
+    server.motdReceivers = [];
 
     // Connect to XMPP servers
-    var client = [];
     client[server.name] = new xmpp.Client({
         jid: server.username + '/bot',
-        password: server.password
+        password: server.password,
+        reconnect: true
+    });
+
+    // Handle client errors
+    client[server.name].on('error', function(err) {
+        util.log("[ERROR] Unknown: " + err);
+    });
+
+    // Handle disconnect
+    client[server.name].on('disconnect', function() {
+        server.rooms.forEach(function(room) {
+            room.joined = false;
+        });
+        util.log("[STATUS] Client disconnected from " + server.name + ". Reconnecting...");
     });
 
     // Once connected, set available presence and join rooms
@@ -123,18 +245,21 @@ config.servers.forEach(function(server) {
             client[server.name].send(new xmpp.Element('presence', { to: roomJID + '/' + server.nickname }).
                 c('x', { xmlns: 'http://jabber.org/protocol/muc' })
             );
-            util.log('[STATUS] Client joined \'' + room.name + '\' on server \'' + server.name + '\'.');
+            util.log("[STATUS] Client joined '" + room.name + "' on " + server.name + ".");
         });
+
+        // Start sending MOTDs
+        timerMOTD(client, server);
     });
 
-    // Parse each stanza from Hatchery
+    // Parse each stanza from the XMPP server
     client[server.name].on('stanza', function(stanza) {
      
         // util.log('***** ' + stanza + ' *****');
 
         // Always log error stanzas
         if (stanza.attrs.type === 'error') {
-            util.log('[ERROR] ' + stanza);
+            util.log("[ERROR] " + stanza);
             return;
         }
      
@@ -150,13 +275,29 @@ config.servers.forEach(function(server) {
                 var room = stanza.attrs.from.split('@')[0];
                 var roomIndex = indexOfRoom(client, server, room);
 
-                if (server.rooms[roomIndex].joined === 'yes' && role !== 'none') {
-                    util.log('[STATUS] User \'' + senderName + '\' joined \'' + room + '\' on \'' + server.name + '\'');
+                if (server.rooms[roomIndex].joined && server.rooms[roomIndex].motd && role !== 'none') {
+                    // Check to see if user is already on list to receive the MOTD.
+                    var existingReceiver = false;
+                    server.motdReceivers.forEach(function(receiver) {
+                        if (receiver.name == senderName) existingReceiver = true;
+                    });
+
+                    // Check to see if user is on the ignore list.
+                    var ignoredReceiver = false;
+                    server.motdIgnore.forEach(function(receiver) {
+                        if (receiver == senderName) ignoredReceiver = true;
+                    });
+
+                    // If new user and not on ignore list, add to MOTD receiver list.
+                    if (! existingReceiver && ! ignoredReceiver) {
+                        server.motdReceivers.push({ name: senderName, joinTime: Math.floor((new Date).getTime() / 1000), sendTime: 0 });
+                    }
+                    util.log("[STATUS] User '" + senderName + "' joined '" + room + "' on " + server.name + ".");
                 }
 
                 // Status code 110 means initial nicklist on room join is complete
                 if (status == "<status code=\"110\"/>") {
-                    server.rooms[roomIndex].joined = 'yes';
+                    server.rooms[roomIndex].joined = true;
                 }
             }
         } else if (stanza.is('message') && stanza.attrs.type === 'groupchat') {
@@ -165,35 +306,31 @@ config.servers.forEach(function(server) {
                -------------------------- */
             var body = stanza.getChild('body');
             // message without body is probably a topic change
-            if (!body) {
+            if (! body) {
                 return;
             }
             
-            var motdadmin = 'no';
+            var motdadmin = false;
             var message = body.getText();
             var sender = stanza.attrs.from.split('/')[1];
             var room = stanza.attrs.from.split('/')[0];
-            var cse = stanza.getChild('cseflags').attrs.cse;
+            if (stanza.getChild('cseflags')) {
+                var cse = stanza.getChild('cseflags').attrs.cse;
+            }
 
             if (cse === "cse") {
-                motdadmin = 'yes';
+                motdadmin = true;
             } else {
                 config.motdAdmins.forEach(function(user) {
                     if (sender === user) {
-                        motdadmin = 'yes';
+                        motdadmin = true;
                     }
                 });
             }
 
-            if (cse === "cse") {
-                util.log('[CHAT-CSE] ' + sender + '@' + server.name + '/' + room.split('@')[0] + ': ' + message);
-            } else {
-                util.log('[CHAT] ' + sender + '@' + server.name + '/' + room.split('@')[0] + ': ' + message);
-            }
-
             // If message matches a defined command, run it
             if (message[0] === commandChar) {
-                var userCommand = message.split(' ')[0].split('!')[1];
+                var userCommand = message.split(' ')[0].split(commandChar)[1];
                 chatCommands.forEach(function(cmd) {
                     if (userCommand === cmd.command) {
                         cmd.exec(client, server, room, sender, message, {motdadmin: motdadmin});
@@ -201,43 +338,52 @@ config.servers.forEach(function(server) {
                 });
             }
 
+            // // Log each message
+            // if (cse === "cse") {
+            //     util.log("[CHAT-CSE] " + sender + "@" + server.name + "/" + room.split('@')[0] + ": " + message);
+            // } else {
+            //     util.log("[CHAT] " + sender + "@" + server.name + "/" + room.split('@')[0] + ": " + message);
+            // }
+
         } else if (stanza.is('message') && stanza.attrs.type === 'chat') {
             /* --------------------------
                Handle private messages
                -------------------------- */
             var body = stanza.getChild('body');
             // message without body is probably a topic change
-            if (!body) {
+            if (! body) {
                 return;
             }
 
-            var motdadmin = 'no';
+            var motdadmin = false;
             var message = body.getText();
             var sender = stanza.attrs.from;
-            var cse = stanza.getChild('cseflags').attrs.cse;
+            if (stanza.getChild('cseflags')) {
+                var cse = stanza.getChild('cseflags').attrs.cse;
+            }
 
             if (cse === "cse") {
-                motdadmin = 'yes';
+                motdadmin = true;
             } else {
                 config.motdAdmins.forEach(function(user) {
                     if (sender.split('@')[0] === user) {
-                        motdadmin = 'yes';
+                        motdadmin = true;
                     }
                 });
             }
 
-            // Log message
-            util.log('[PM] ' + sender + '@' + server.name + ': ' + message);
-
             // If message matches a defined command, run it
             if (message[0] === commandChar) {
-                var userCommand = message.split(' ')[0].split('!')[1];
+                var userCommand = message.split(' ')[0].split(commandChar)[1];
                 chatCommands.forEach(function(cmd) {
                     if (userCommand === cmd.command) {
                         cmd.exec(client, server, 'pm', sender, message, {motdadmin: motdadmin});
                     }
                 });
             }
+
+            // // Log each message
+            // util.log("[PM] " + sender + "@" + server.name + ": " + message);
 
         } else {
             /* --------------------------
@@ -247,18 +393,3 @@ config.servers.forEach(function(server) {
         }
     });
 });
-
-
-
-
-
-
-
-
-// fs.writeFile("/tmp/test", "Hey there!", function(err) {
-//   if(err) {
-//       return console.log(err);
-//   }
-
-//   console.log("The file was saved!");
-// }); 
